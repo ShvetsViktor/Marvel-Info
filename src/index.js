12fe1a71360db05d00456f4329db368fb8d79e33
viktor_shvets <shvetsviktor89@gmail.com>
2025-03-25 13:27:51 +0000

    lesson 12 Redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
+import React from "react";
+
 import "./style/style.scss";
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+
+);
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,7 @@
 class MarvelService {
+    _apiBase = 'https://marvel-server-zeta.vercel.app/';
+    _apiKey = 'apikey=d4eecb0c66dedbfae4eab45d312fc1df';
+
     getResource = async (url) => {
 
         let res = await fetch(url);
@@ -11,12 +14,31 @@ class MarvelService {
     }
 
     getAllCharacters = () => {
-        return this.getResource();
+        return this.getResource('https://marvel-server-zeta.vercel.app/characters?apikey=d4eecb0c66dedbfae4eab45d312fc1df');
+    }
+
+    // getAllCharacters = async () => {
+    //     const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    //     return res.data.results.map(this._transformCharacter);
+    // }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 
 
-
-https://gateway.marvel.com:443/v1/public/characters?apikey=c5d6fc8b83116d92ed468ce36bac6c62
-
-
 }
+
+
+export default MarvelService;
