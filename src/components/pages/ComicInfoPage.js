0f5f44cd2c8f2edd8af0a19b9fe310a8045ecbbc
Viktor <shvetsviktor89@gmail.com>
2025-06-12 13:49:19 +0100

    comicsList.scss edited

--- src/components/pages/ComicInfoPage.js
+++ src/components/pages/ComicInfoPage.js
@@ -3,14 +3,14 @@ import AppBanner from '../appBanner/AppBanner';
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 
-const ComicInfoPage = () => {
+const ComicInfoPage = (props) => {
 
 
     return (
         <>
             <AppBanner />
             <ErrorBoundary>
-                <ComicInfo comicId={1} />
+                <ComicInfo comicId={props} />
             </ErrorBoundary>
         </>
     )
