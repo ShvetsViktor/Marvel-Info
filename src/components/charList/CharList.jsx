c51eded296f4e58ec619143649606ac75b596db4
Viktor <shvetsviktor89@gmail.com>
2025-07-04 10:18:16 +0100

    no-image error handling added + image-placeholder.png added to assets

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,4 +1,4 @@
-import React ,{ useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import useMarvelService from '../../services/MarvelService';
@@ -14,8 +14,8 @@ const CharList = (props) => {
     const newItemLoading = useRef(false);
     const charEndedRef = useRef(false);
 
-    const {loading, error, getAllCharacters} = useMarvelService();
-    
+    const { loading, error, getAllCharacters } = useMarvelService();
+
     useEffect(() => {
         onRequest(offsetRef.current, true);
 
@@ -25,7 +25,7 @@ const CharList = (props) => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, [])
-    
+
     const onRequest = (offset, initial) => {
         if (charEnded || newItemLoading.current) return;
 
@@ -42,7 +42,7 @@ const CharList = (props) => {
         setCharList(prevCharList => {
             const existingIds = new Set(prevCharList.map(char => char.id));
             const filteredNewList = newCharList.filter(char => !existingIds.has(char.id));
-  
+
             const updatedList = [...prevCharList, ...filteredNewList];
             offsetRef.current = updatedList.length;
 
@@ -54,15 +54,15 @@ const CharList = (props) => {
             return updatedList;
         });
     }
-    
+
     const itemRefs = useRef([]);
-   
+
     const focusOnItem = (i) => {
         itemRefs.current.forEach((item) => item.classList.remove('char__item_selected'));
         itemRefs.current[i].classList.add('char__item_selected');
         itemRefs.current[i].focus();
     }
-    
+
     const offsetRef = useRef(0);
 
     const handleScroll = () => {
@@ -77,7 +77,7 @@ const CharList = (props) => {
     function renderItems(arr) {
         const items = arr.map((item, i) => {
             return (
-                <li 
+                <li
                     className="char__item"
                     tabIndex={0}
                     ref={el => itemRefs.current[i] = el}
@@ -90,16 +90,23 @@ const CharList = (props) => {
                         if (e.key === ' ' || e.key === 'Enter') {
                             e.preventDefault();
                             props.onCharSelected(item.id);
-                            focusOnItem(i); 
+                            focusOnItem(i);
                         }
                     }}>
-                        <img src={item.thumbnail} alt={item.name}/>
-                        <div className="char__name">{item.name}</div>
+                    <img
+                        src={item.thumbnail}
+                        alt={item.name}
+                        onError={(e) => {
+                            e.target.onerror = null;
+                            e.target.src = '/assets/placeholder.png';
+                        }}
+                    />
+                    <div className="char__name">{item.name}</div>
                 </li>
             )
         })
 
-        return(
+        return (
             <ul className="char__grid">
                 {items}
             </ul>
@@ -107,18 +114,18 @@ const CharList = (props) => {
     }
 
     const items = renderItems(charList);
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading && !newItemLoading.current ? <Spinner/> : null;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading && !newItemLoading.current ? <Spinner /> : null;
 
     return (
         <div className="char__list">
             {errorMessage}
             {spinner}
             {items}
-            <button 
+            <button
                 className="button button__main button__long"
                 disabled={newItemLoading.current}
-                style={{'display' : charEnded ? 'none' : 'block'}}
+                style={{ 'display': charEnded ? 'none' : 'block' }}
                 onClick={() => {
                     onRequest(offsetRef.current, false);
                 }}>
